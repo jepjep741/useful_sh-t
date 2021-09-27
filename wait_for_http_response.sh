c5f6759612ba8faea975b024e7ed6d09ee5304c2
jepjep741 <89215038+jepjep741@users.noreply.github.com>
2021-09-27 14:39:13 +0300

    The initial commit

---
+++ wait_for_http_response.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TIMEOUT="${TIMEOUT:-600}"
+seconds=0
+URL=http://localhost:80
+
+
+echo 'Waiting up to' $TIMEOUT 'seconds for HTTP 200 from' $URL 
+until [ "$seconds" -gt "$TIMEOUT" ] || $(curl --output /dev/null --silent --max-time $TIMEOUT --head --fail $URL); do
+  printf '.'
+  sleep 5
+  seconds=$((seconds+5))
+done
+
+if [ "$seconds" -lt "$TIMEOUT" ]; then
+  echo 'OK'
+### Excute stuff
+
+else
+  echo "ERROR: Timed out wating for HTTP 200 from" $URL >&2
+  exit 1
+fi
